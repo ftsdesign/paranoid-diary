3901255a95c79db9a330300aff9ff63c6b03ca0b
ftsdesign <ftsdesign@ftsdesign.biz>
2020-04-18 14:51:06 +0800

    Datetime formats cleanup

--- app/src/main/java/biz/ftsdesign/paranoiddiary/Formats.java
+++ app/src/main/java/biz/ftsdesign/paranoiddiary/Formats.java
@@ -1,12 +1,25 @@
 package biz.ftsdesign.paranoiddiary;
 
+import android.annotation.SuppressLint;
+
+import androidx.annotation.NonNull;
+
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public abstract class Formats {
+    @SuppressLint("SimpleDateFormat")
     public static final SimpleDateFormat TIMESTAMP_FORMAT = new SimpleDateFormat("dd-MMM-yyyy, EEE HH:mm");
-    public static final SimpleDateFormat FILE_TIMESTAMP_FORMAT = new SimpleDateFormat("yyyyMMdd_HHmm");
+    @SuppressLint("SimpleDateFormat")
+    static final SimpleDateFormat FILE_TIMESTAMP_FORMAT = new SimpleDateFormat("yyyyMMdd_HHmm");
+    @SuppressLint("SimpleDateFormat")
+    static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd-MMM-yyyy");
 
     private Formats() {
         throw new UnsupportedOperationException();
+    }
+
+    public static String format(@NonNull SimpleDateFormat format, long time) {
+        return format.format(new Date(time));
     }
 }
--- app/src/main/java/biz/ftsdesign/paranoiddiary/RecordHeaderFormat.java
+++ app/src/main/java/biz/ftsdesign/paranoiddiary/RecordHeaderFormat.java
@@ -1,23 +1,16 @@
 package biz.ftsdesign.paranoiddiary;
 
-import java.util.Date;
-
 import biz.ftsdesign.paranoiddiary.model.Record;
+
+import static biz.ftsdesign.paranoiddiary.Formats.TIMESTAMP_FORMAT;
 
 abstract class RecordHeaderFormat {
     abstract String format(Record record);
 
-    static final RecordHeaderFormat FORMAT_DEV = new RecordHeaderFormat() {
-        @Override
-        String format(Record record) {
-            return "#" + record.getId() + " " + Formats.TIMESTAMP_FORMAT.format(new Date(record.getTimeCreated()));
-        }
-    };
-
     static final RecordHeaderFormat FORMAT_DEFAULT = new RecordHeaderFormat() {
         @Override
         String format(Record record) {
-            return Formats.TIMESTAMP_FORMAT.format(new Date(record.getTimeCreated()));
+            return Formats.format(TIMESTAMP_FORMAT, record.getTimeCreated());
         }
     };
 }

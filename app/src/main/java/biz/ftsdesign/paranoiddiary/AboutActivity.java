32b4567a65318cb8d82824f5ec3590f8101656b6
ftsdesign <ftsdesign@ftsdesign.biz>
2021-08-08 13:04:28 +0800

    Version number added to About activity

--- app/src/main/java/biz/ftsdesign/paranoiddiary/AboutActivity.java
+++ app/src/main/java/biz/ftsdesign/paranoiddiary/AboutActivity.java
@@ -2,6 +2,8 @@ package biz.ftsdesign.paranoiddiary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
@@ -14,10 +16,24 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
+        TextView version = findViewById(R.id.textViewVersion);
+        version.setText(getVersionString());
+
         TextView about = findViewById(R.id.textViewAbout);
         about.setText(Html.fromHtml(getString(R.string.about_text)));
         about.setMovementMethod(new LinkMovementMethod());
         about.setFocusable(true);
         about.setTextIsSelectable(true);
+    }
+
+    private String getVersionString() {
+        try {
+            PackageInfo pInfo = getApplicationContext().getPackageManager().getPackageInfo(getApplicationContext().getPackageName(), 0);
+            String versionName = pInfo.versionName;
+            int versionCode = pInfo.versionCode;
+            return getString(R.string.version_info, versionName, versionCode);
+        } catch (PackageManager.NameNotFoundException e) {
+            return getString(R.string.version_unknown);
+        }
     }
 }

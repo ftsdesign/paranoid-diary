7d3fdc6c00c4a28ddecd7ee4c00856c2b7383602
ftsdesign <ftsdesign@ftsdesign.biz>
2022-05-09 09:13:11 +0800

    Don't print tag names in the log

--- app/src/main/java/biz/ftsdesign/paranoiddiary/Util.java
+++ app/src/main/java/biz/ftsdesign/paranoiddiary/Util.java
@@ -71,7 +71,7 @@ public class Util {
     @SuppressLint("SetTextI18n")
     static void setTagsBox(@NonNull Activity activity, @NonNull FlexboxLayout tagsBox,
                            @NonNull Record record, @Nullable RecordPredicateListener recordPredicateListener) {
-        Log.d(Util.class.getSimpleName(), "setTagsBox " + record + " " + record.getTags());
+        Log.d(Util.class.getSimpleName(), "setTagsBox " + record + " " + record.getTags().size() + " tags");
         tagsBox.removeAllViewsInLayout();
         if (!record.getTags().isEmpty()) {
             int index = 0;
